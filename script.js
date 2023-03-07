f39352ec116c54e74a028e506bd07ec9d41712a5
Mariesly <marieslyaguilar@gmail.com>
2023-03-07 11:22:49 -0400

    suma simple

---
+++ script.js
@@ -0,0 +1,6 @@
+let numero1=document.querySelector('#numero1');
+let numero2=document.querySelector('#numero2');
+
+function calculando(){
+    console.log(Number(numero1.value) + Number(numero2.value))
+}
