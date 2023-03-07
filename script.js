53d18d6e00484333a6fafeeb5b4eaf3736b58dc5
Mariesly <marieslyaguilar@gmail.com>
2023-03-07 16:13:33 -0400

    suma simple lista

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let numero1=document.querySelector('#numero1');
 let numero2=document.querySelector('#numero2');
+let pResultado=document.querySelector('#resultado')
 
 function calculando(){
-    console.log(Number(numero1.value) + Number(numero2.value))
+    let Suma= (Number(numero1.value) + Number(numero2.value))
+    pResultado.innerHTML= 'Resultado es: ' + Suma;
+
 }
