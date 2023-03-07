c46fd19781aa7e37f86671731a04043cd370f46f
Mariesly <marieslyaguilar@gmail.com>
2023-03-07 17:21:36 -0400

    cambiando js funcion addEventListener

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let numero1=document.querySelector('#numero1');
 let numero2=document.querySelector('#numero2');
 let pResultado=document.querySelector('#resultado')
+const adn=document.querySelector('#calculo')
+
+adn.addEventListener('click',calculando);
 
 function calculando(){
     let Suma= (Number(numero1.value) + Number(numero2.value))
