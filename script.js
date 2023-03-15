7043632503bff1a2d25ed300bf2d5524c52e39b0
Mariesly <marieslyaguilar@gmail.com>
2023-03-14 21:46:18 -0400

    cambios no se que

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let numero1=document.querySelector('#numero1');
+let form=document.querySelector('#form');
 let numero2=document.querySelector('#numero2');
 let pResultado=document.querySelector('#resultado')
 const adn=document.querySelector('#calculo')
 
-adn.addEventListener('click',calculando);
+form.addEventListener('submit',calculando);
 
-function calculando(){
+function calculando(event){
+    event.preventDefault();
     let Suma= (Number(numero1.value) + Number(numero2.value))
     pResultado.innerHTML= 'Resultado es: ' + Suma;
 
